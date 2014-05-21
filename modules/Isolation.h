5fad4d7b503d164879f2053ea5d1e7c8441e3e3e
Sho Iwamoto <sho.iwamoto@ipmu.jp>
2014-05-21 18:20:47 +0900

    Delphes 3.0.10

---
+++ modules/Isolation.h
@@ -0,0 +1,66 @@
+#ifndef Isolation_h
+#define Isolation_h
+
+/** \class Isolation
+ *
+ *  Sums transverse momenta of isolation objects (tracks, calorimeter towers, etc)
+ *  within a DeltaR cone around a candidate and calculates fraction of this sum
+ *  to the candidate's transverse momentum. outputs candidates that have
+ *  the transverse momenta fraction within (PTRatioMin, PTRatioMax].
+ *
+ *  $Date: 2013-08-19 15:40:54 +0200 (Mon, 19 Aug 2013) $
+ *  $Revision: 1267 $
+ *
+ *
+ *  \author P. Demin - UCL, Louvain-la-Neuve
+ *
+ */
+
+#include "classes/DelphesModule.h"
+
+class TObjArray;
+
+class ExRootFilter;
+class IsolationClassifier;
+
+class Isolation: public DelphesModule
+{
+public:
+
+  Isolation();
+  ~Isolation();
+
+  void Init();
+  void Process();
+  void Finish();
+
+private:
+
+  Double_t fDeltaRMax;
+
+  Double_t fPTRatioMax;
+
+  Double_t fPTSumMax;
+
+  Bool_t fUsePTSum;
+
+  IsolationClassifier *fClassifier; //!
+
+  ExRootFilter *fFilter;
+
+  TIterator *fItIsolationInputArray; //!
+
+  TIterator *fItCandidateInputArray; //!
+
+  const TObjArray *fIsolationInputArray; //!
+
+  const TObjArray *fCandidateInputArray; //!
+
+  const TObjArray *fRhoInputArray; //!
+
+  TObjArray *fOutputArray; //!
+
+  ClassDef(Isolation, 1)
+};
+
+#endif
---
+++ readme.md
@@ -0,0 +1,19 @@
+[Delphes3](https://cp3.irmp.ucl.ac.be/projects/delphes) 
+=========
+
+### Authors
+
+   [The DelphesTeam](https://cp3.irmp.ucl.ac.be/projects/delphes/wiki/TheTeam)
+   
+
+### References
+
+ * [Comput. Phys. Commun. **153** (2003) 275](http://dx.doi.org/10.1016/S0010-4655(03)00222-4) [[hep-ph/0301101](http://arxiv.org/abs/hep-ph/0301101)].
+ * https://spheno.hepforge.org/
+
+Note that the following packages are included as externals.
+
+  * [ExRootAnalysis](https://cp3.irmp.ucl.ac.be/projects/ExRootAnalysis),
+  * [FastJet](http://fastjet.fr/) by Cacciari, Salam and Soyez ([1111.6097](http://arxiv.org/abs/1111.6097)),
+  * [ProMC](http://atlaswww.hep.anl.gov/asc/promc/) by S. Chekanov et al. ([1306.6675](http://arxiv.org/abs/1306.6675), [1311.1229](http://arxiv.org/abs/1311.1229)),
+  * subset (?) of [Tcl C API](http://wiki.tcl.tk/14054).
